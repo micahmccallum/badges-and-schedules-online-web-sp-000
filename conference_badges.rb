826e888fd3c8fdbc7528a296956dfe5ba508e3cb
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 00:34:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,17 @@ def batch_badge_creator(array)
   array.collect { |name| "Hello, my name is #{name}."}
 end
 
-def assign_rooms(array)
 
+
+def assign_rooms(array)
+  assignments = []
+  array.each_with_index do |name, i+1|
+    assignments.push("Hello, #{name}!, You'll be assigned to room #{}")
+
+  end
+end
+
+def printer(array)
+  array.each
+    puts batch_badge_creator(array), assign_rooms(array)
 end
