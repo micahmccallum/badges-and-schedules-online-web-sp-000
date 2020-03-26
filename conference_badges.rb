00a76b7012b2f468ab2a87f8cb84ce76b471159b
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 00:03:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,6 @@
 require 'pry'# Write your code here.
 def badge_maker(array)
   badges = []
-  binding.pry
-  array.each do |name|
-
-    badges.push("Hello, my name is #{name}.")
-  end
+  badges.push("Hello, my name is #{name}.")
   badges
 end
