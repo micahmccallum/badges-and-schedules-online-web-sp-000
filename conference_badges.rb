feab23b4a9ac55240438eed30a0ed11d751b93d0
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 00:50:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect { |name| "Hello, my name is #{name}."}
+end
+
+
+
+def assign_rooms(array)
+  assignments = []
+  array.each_with_index do |name, i|
+    assignments.push("Hello, #{name}! You'll be assigned to room #{i + 1}!")
+  end
+  assignments
+end
+
+def printer(array)
+  array.each_with_index do |name, index|
+    puts batch_badge_creator(array)[index]
+    puts assign_rooms(array)[index]
+  end
+end
